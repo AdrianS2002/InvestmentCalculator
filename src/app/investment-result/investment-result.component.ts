3feff67f8304a1cb07d46c0206ebd202048e5e47
AdrianS2002 <aschiop2002@yahoo.com>
2024-12-02 18:20:42 +0200

    added project

---
+++ src/app/investment-result/investment-result.component.ts
@@ -0,0 +1,30 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-result.component.html',
+  styleUrl: './investment-result.component.css',
+})
+export class InvestmentResultComponent {
+
+  //results = input<{
+  //   year: number,
+  //   interest: number,
+  //   valueEndOfYear: number,
+  //   annualInvestment: number,
+  //   totalInterest: number,
+  //   totalAmountInvested: number,
+  // }[]>();
+  @Input() results?: {
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number,
+  }[];
+
+}
